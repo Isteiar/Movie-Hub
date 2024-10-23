00f34b4657deca408f8cb040526877273f758f91
Isteiar <isteiarahmed8@egmail.com>
2024-10-23 15:34:09 +0600

    DB is connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectedToDB } from "./config/db";
+dotenv.config();
+
+const PORT: number = parseInt(process.env.PORT as string) || 4000;
 
 const app = express();
 
-app.listen(5000, () =>
-  console.log(`Server is runnig on http://localhost:5000`)
-);
+app.use(express.json());
+
+(async () => {
+  try {
+    await connectedToDB();
+    console.log("DB is connected");
+
+    app.listen(PORT, () =>
+      console.log(`Server is runnig on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log("Server is not connected");
+  }
+})();
